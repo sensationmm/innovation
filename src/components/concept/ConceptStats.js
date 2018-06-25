9643c15693ec3507e5f569b97761097417a50a2a
Kevin Reynolds <kevin.reynolds@nimbletank.com>
2018-06-25 17:18:35 +0100

    Concept one pager v2

--- src/components/concept/ConceptStats.js
+++ src/components/concept/ConceptStats.js
@@ -11,36 +11,24 @@ import '../../styles/css/concept-meta.css';
  *
  * @param {string} label - label for the panel
  * @param {array} stats - array of label / content pairs
- * @param {string} summary - optional text to show above or instead of stats
  * @param {boolean} stacked - if false will render in 2 columns, if true will be single column
- * @param {boolean} isStats - if true will render the stat value in larger font
  */
 
 const ConceptMeta = props => {
-  const { label, stats, summary, stacked, isStats } = props;
+  const { label, stats, stacked } = props;
 
   return (
-    <div className={classnames('concept-meta', {stacked: stacked & !isStats}, {stats: isStats})}>
+    <div className={classnames('concept-meta', {stacked: stacked})}>
       <div className="concept-meta-label">{label}</div>
-      {summary && 
-        <div className="concept-meta-summary">{summary}</div>
-      }
       <div className="concept-meta-stats">
       {
         stats.map((stat, count) => {
-
-          const markup = [
-            <div key={`stat-${count}`} className="concept-meta-stat slider-item">
+          return (
+            <div className="concept-meta-stat" key={`stat-${count}`}>
               <div className="concept-meta-stat-label">{stat.label}</div>
               <div className="concept-meta-stat-content">{stat.content}</div>
             </div>
-          ];
-
-          if(count < stats.length - 1) {
-            markup.push(<div key={`spacer-${count}`} className="concept-meta-spacer" />);
-          }
-
-          return markup
+          )
         })
       }
       </div>
@@ -51,16 +39,11 @@ const ConceptMeta = props => {
 ConceptMeta.propTypes = {
   label: PropTypes.string,
   stats: PropTypes.array,
-  summary: PropTypes.string,
-  stacked: PropTypes.bool,
-  isStats: PropTypes.bool
+  stacked: PropTypes.bool
 };
 
 ConceptMeta.defaultProps = {
-  stacked: false,
-  isStats: false,
-  stats: [],
-  summary: ''
+  stacked: false
 };
 
 export default ConceptMeta;

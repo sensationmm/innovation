1a04d737009670ff6ee0a4f42e0aeb5289e87056
Kevin Reynolds <kevin.reynolds@nimbletank.com>
2018-07-13 17:04:55 +0100

    lint error 2

--- src/containers/Canvas.js
+++ src/containers/Canvas.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 // import PropTypes from 'prop-types';
 
@@ -9,63 +9,60 @@ import CanvasGrid from '../components/CanvasGrid';
 
 import '../styles/css/canvas.css';
 
-class Canvas extends Component {
+const Canvas = () => {
+  return (
+    <div className="canvas">
+      <FlexRow layout={['25%','35%','40%']}>
+        <FlexColumn>
+          <ContentBox>
+            <div className="canvas-grid-header">Who is it for?</div>
+            <CanvasGrid />
+          </ContentBox>
+          <ContentBox>
+            <div className="canvas-grid-header">What is the pain point / opportunity?</div>
+            <CanvasGrid />
+          </ContentBox>
+          <ContentBox>
+            <div className="canvas-grid-header">Relevant technologies</div>
+            <CanvasGrid />
+          </ContentBox>
+        </FlexColumn>
 
-  render() {
-    return (
-      <div className="canvas">
-        <FlexRow layout={['25%','35%','40%']}>
-          <FlexColumn>
-            <ContentBox>
-              <div className="canvas-grid-header">Who is it for?</div>
-              <CanvasGrid />
-            </ContentBox>
-            <ContentBox>
-              <div className="canvas-grid-header">What is the pain point / opportunity?</div>
-              <CanvasGrid />
-            </ContentBox>
-            <ContentBox>
-              <div className="canvas-grid-header">Relevant technologies</div>
-              <CanvasGrid />
-            </ContentBox>
-          </FlexColumn>
+        <FlexColumn>
+          <ContentBox>
+            <div className="canvas-grid-header">What is the idea?</div>
+            <CanvasGrid />
+          </ContentBox>
+          <ContentBox>
+            <div className="canvas-grid-header">How does it work?</div>
+            <CanvasGrid />
+          </ContentBox>
+        </FlexColumn>
 
-          <FlexColumn>
+        <FlexColumn layout={['55%','22.5%','22.5%']}>
+          <ContentBox>
+            <div className="canvas-grid-header">Storyboard</div>
+            <CanvasGrid />
+          </ContentBox>
+          <FlexRow>
             <ContentBox>
-              <div className="canvas-grid-header">What is the idea?</div>
+              <div className="canvas-grid-header">For the person / people</div>
               <CanvasGrid />
             </ContentBox>
             <ContentBox>
-              <div className="canvas-grid-header">How does it work?</div>
+              <div className="canvas-grid-header">For the company</div>
               <CanvasGrid />
             </ContentBox>
-          </FlexColumn>
+          </FlexRow>
+          <ContentBox>
+            <div className="canvas-grid-header">How does it align to the companys aspirations?</div>
+            <CanvasGrid />
+          </ContentBox>
+        </FlexColumn>
 
-          <FlexColumn layout={['55%','22.5%','22.5%']}>
-            <ContentBox>
-              <div className="canvas-grid-header">Storyboard</div>
-              <CanvasGrid />
-            </ContentBox>
-            <FlexRow>
-              <ContentBox>
-                <div className="canvas-grid-header">For the person / people</div>
-                <CanvasGrid />
-              </ContentBox>
-              <ContentBox>
-                <div className="canvas-grid-header">For the company</div>
-                <CanvasGrid />
-              </ContentBox>
-            </FlexRow>
-            <ContentBox>
-              <div className="canvas-grid-header">How does it align to the company's aspirations?</div>
-              <CanvasGrid />
-            </ContentBox>
-          </FlexColumn>
-
-        </FlexRow>
-      </div>
-    );
-  }
+      </FlexRow>
+    </div>
+  );
 }
 
 Canvas.propTypes = {

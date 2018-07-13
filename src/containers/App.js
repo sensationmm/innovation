fc34ae62639178dfad8762e4b1b04daed1409653
ObserverMoment <bosonsoul@gmail.com>
2018-07-13 15:16:25 +0100

    merge

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import ConceptV2 from './ConceptV2';
 import InnovationCreate from './InnovationCreate';
-import InnovationDashboard from './InnovationDashboard';
+// import InnovationDashboard from './InnovationDashboard';
 import ConceptCreate from './ConceptCreate';
 import Grouping from './Grouping';
 import Tracking from './Tracking';
@@ -22,7 +22,7 @@ const App = () => (
       <Switch>
         <PrivateRoute exact path="/" component={Home} />
         <PrivateRoute exact path="/concept/create" component={ConceptCreate} />
-        <PrivateRoute exact path="/innovation/dashboard" component={InnovationDashboard} />
+        {/* <PrivateRoute exact path="/innovation/dashboard" component={InnovationDashboard} /> */}
         <PrivateRoute exact path="/innovation/create" component={InnovationCreate} />
         <PrivateRoute exact path="/grouping" component={Grouping} />
         <PrivateRoute exact path="/tracking" component={Tracking} />

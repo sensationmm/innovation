d8176fbd6f557b943228be7d1a674ccfbc62af4d
ObserverMoment <bosonsoul@gmail.com>
2018-07-30 16:12:25 +0100

    remove env=development condition on redux dev tools

--- src/store.js
+++ src/store.js
@@ -12,12 +12,10 @@ const initialState = {};
 const enhancers = [];
 const middleware = [thunk, routerMiddleware(history)];
 
-if (process.env.NODE_ENV === 'development') {
-  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
 
-  if (typeof devToolsExtension === 'function') {
+if (typeof devToolsExtension === 'function') {
     enhancers.push(devToolsExtension());
-  }
 }
 
 const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);

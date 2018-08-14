4cf247e1356c7540eb37ecc1fe3a55bd55d21c62
ObserverMoment <bosonsoul@gmail.com>
2018-08-14 11:36:39 +0100

    capitals fixe 3

--- src/components/concept/ConceptOverviewRead.js
+++ src/components/concept/ConceptOverviewRead.js
@@ -50,7 +50,7 @@ const ConceptOverviewRead = (props) => {
               key={`concept-info-${fieldGroup.key}`}
               label={fieldGroup.displayAs}
               stats={fieldGroup.contents.map(item => {
-                return item.value === 'businessType' || item.value === 'salesChannel'
+                return item.value === 'businessType' || item.value === 'salesChannel' || item.value === 'willGmLeave'
                   ? {
                     label: item.label,
                     content: item.value === 'salesChannel'
